378369a7fdbac2eac237870cce0d9476d8fd3361
Lidor <lidor115@gmail.com>
2019-04-14 23:24:16 +0300

    singleton and command

---
+++ FlightSimulator/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulator
+{
+    class Singleton
+    {
+        private Commands _singleton;
+        public Commands singleton
+        {
+            get
+            {
+                if(_singleton == null)
+                {
+                    _singleton = new Commands();
+                }
+                return _singleton;
+            }
+        }
+    }
+}
