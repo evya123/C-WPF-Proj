3b8c335367ddf189c5f34ee8e798725cd1818bcc
Lidor <lidor115@gmail.com>
2019-04-16 00:35:12 +0300

    AutoVM

--- FlightSimulator/ViewModels/AutoPilotVM.cs
+++ FlightSimulator/ViewModels/AutoPilotVM.cs
@@ -3,14 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+
 
 namespace FlightSimulator.ViewModels
 {
-    class AutoPilotVM:BaseNotify
+    class AutoPilotVM : BaseNotify
     {
-        public AutoPilotVM()
+        private List<List<string>> AllCommands = new List<List<string>>();
+        private ICommand _connect;
+        private ICommand _clear;
+        // check if the user write or not in the textbox
+        private bool isWrite =false;
+        private string color;
+        private String data = "";
+        private string blank = "";
+        public String ChangeColor
         {
+            get
+            {
+                if (blank != "") 
+                {
+                    color = "Pink";
+                }
+                else
+                {
+                    color = "White";
+                }
+                return color;
+            }
+        }
+        public String CommandsFromUser
+        {
+            set
+            {
+                this.data = value;
+                blank = value;
+                isWrite = true;
+                NotifyPropertyChanged("CommandsFromUser");
+                NotifyPropertyChanged("ChangeColor");
+
+            }
+            get
+            {
+                if (!isWrite)
+                {
+                    NotifyPropertyChanged("ChangeColor");
+                }
+                return data;
+            }
             
         }
+
+        public void OnClick()
+        {
+            blank = "";
+        }
+
     }
 }
